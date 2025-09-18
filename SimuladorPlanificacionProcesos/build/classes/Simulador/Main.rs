9a4a01fd2475e54cdb5d10dee087d401b51df2bf
OdalisCQ <mcajbonq@miumg.edu.gt>
2025-09-18 12:26:43 -0600

    Agregue la estructura del proyecto

---
+++ SimuladorPlanificacionProcesos/build/classes/Simulador/Main.rs
@@ -0,0 +1,1 @@
+simulador.Main
---
+++ SimuladorPlanificacionProcesos/src/Simulador/Main.java
@@ -0,0 +1,10 @@
+
+
+
+package simulador;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Simulador de Planificación de Procesos iniciado...");
+    }
+}
